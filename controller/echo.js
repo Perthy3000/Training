537e8f07ed8240ac985093c2dc0eb76de457f4cd
Nattakorn <perth.ploy@gmail.com>
2021-06-05 18:50:31 +0700

    change response body

--- controller/echo.js
+++ controller/echo.js
@@ -2,19 +2,19 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/echo_get", (req, res) => {
-    res.json({ message: "Echo from the other side" });
+  res.json({ message: "Echo from the other side" });
 });
 
 router.get("/echo_params/:param", (req, res) => {
-    res.json({ params: req.params.param });
+  res.json({ params: req.params.param });
 });
 
 router.get("/echo_qs", (req, res) => {
-    res.json(req.query);
+  res.json(req.query);
 });
 
 router.post("/", (req, res) => {
-    res.json({ test: "success" });
+  res.json(req.body);
 });
 
 module.exports = router;
--- controller/todo.js
+++ controller/todo.js
@@ -3,56 +3,56 @@ const router = express.Router();
 const Todo = require("../db/schema/todo");
 
 router.get("/todos", async (req, res) => {
-    try {
-        const todoList = await Todo.find({});
-        res.json({ success: true, count: todoList.length, data: todoList });
-    } catch (err) {
-        res.status(400).json({ error: "There is an error", data: {} });
-    }
+  try {
+    const todoList = await Todo.find({});
+    res.json({ success: true, count: todoList.length, data: todoList });
+  } catch (err) {
+    res.status(400).json({ error: err.message, data: {} });
+  }
 });
 
 router.get("/todos/:_id", async (req, res) => {
-    const todo = await Todo.find({ _id: req.params._id });
-    if (todo.length == 0) {
-        res.status(400).json({ error: "todo not found", data: {} });
-    } else {
-        res.json({ success: true, data: todo });
-    }
+  const todo = await Todo.find({ _id: req.params._id });
+  if (todo.length == 0) {
+    res.status(400).json({ error: "todo not found", data: {} });
+  } else {
+    res.json({ success: true, data: todo });
+  }
 });
 
 router.post("/todos", async (req, res) => {
-    try {
-        const maxValue = await Todo.find({}).sort({ order: -1 }).limit(1);
-        const todo = await Todo.create({
-            title: req.body.title,
-            order: maxValue.length > 0 ? maxValue[0].order + 1 : 1,
-        });
-        res.json({ success: true, data: todo });
-    } catch (err) {
-        res.status(400).json({ error: "There is an error", data: {} });
-    }
+  try {
+    const maxValue = await Todo.find({}).sort({ order: -1 }).limit(1);
+    const todo = await Todo.create({
+      title: req.body.title,
+      order: maxValue.length > 0 ? maxValue[0].order + 1 : 1,
+    });
+    res.json({ success: true, data: todo });
+  } catch (err) {
+    res.status(400).json({ error: "There is an error", data: {} });
+  }
 });
 
 router.put("/todos/:_id", async (req, res) => {
-    const todo = await Todo.findOneAndUpdate(
-        { _id: req.params._id },
-        { title: req.body.title },
-        { new: true }
-    );
-    if (!todo) {
-        res.status(400).json({ error: "todo not found", data: {} });
-    } else {
-        res.json({ success: true, data: todo });
-    }
+  const todo = await Todo.findOneAndUpdate(
+    { _id: req.params._id },
+    { title: req.body.title },
+    { new: true }
+  );
+  if (!todo) {
+    res.status(400).json({ error: "todo not found", data: {} });
+  } else {
+    res.json({ success: true, data: todo });
+  }
 });
 
 router.delete("/todos/:_id", async (req, res) => {
-    const todo = await Todo.deleteOne({ _id: req.params._id });
-    if (todo.n == 0) {
-        res.status(400).json({ error: "todo not found", data: {} });
-    } else {
-        res.json({ success: true, data: {} });
-    }
+  const todo = await Todo.deleteOne({ _id: req.params._id });
+  if (todo.n == 0) {
+    res.status(400).json({ error: "todo not found", data: {} });
+  } else {
+    res.json({ success: true, data: {} });
+  }
 });
 
 module.exports = router;
