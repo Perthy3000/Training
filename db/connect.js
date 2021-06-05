4df9a65e77aa542b98d91210ce507ed289ec0cd9
Nattakorn <perth.ploy@gmail.com>
2021-06-05 18:48:55 +0700

    remove callback function

--- db/connect.js
+++ db/connect.js
@@ -1,18 +1,15 @@
 const mongoose = require("mongoose");
 const { DATABASE_URL } = require("./config");
 
-const connect_db = (appStart) => {
-    mongoose
-        .connect(DATABASE_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        })
-        .then(() => {
-            console.log("Connected to database!");
-        })
-        .then(() => {
-            if (appStart) appStart();
-        });
+const connect_db = () => {
+  mongoose
+    .connect(DATABASE_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("Connected to database!");
+    });
 };
 
 module.exports = connect_db;
